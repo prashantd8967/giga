ea5b59f1937210bd1d83db1c2e94eee92cbc030c
prashant <prashant.dubey@tothenew.com>
2022-09-27 16:59:44 +0530

    done the java program

---
+++ Documents/java_program/Firstasign.java
@@ -0,0 +1,20 @@
+public class Firstasign{
+	
+	static int age = 23;
+	static {
+		System.out.println("Prashant");
+	}
+
+	public static void get_lastname() {
+		System.out.println("Dubey");
+
+	}
+
+	public static void main(String[] args) {
+		get_lastname();
+
+		System.out.println(Firstasign.age);
+
+	}
+
+}
